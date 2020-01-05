6c890f945266ccfaa16cb5dc53896e60be3face5
Valentina <valentinalobodane@gmail.com>
2020-01-05 15:31:00 +0200

    add cycle for inputs

--- main13.js
+++ main13.js
@@ -1,5 +1,7 @@
 window.onload = function () {
     var inputs = document.querySelectorAll('input');
     var area = document.querySelector('textarea');
-    
+    for (var i = 0;i < inputs.length; i++) {
+        inputs[i].buffer = "";
+    }
 }
