137d6bbd1ab7f82ccdcc4426956d4c4d9d98aec4
Valentina <valentinalobodane@gmail.com>
2020-01-12 16:30:20 +0200

    add style body and border box, add function onclick for box

--- main13.js
+++ main13.js
@@ -1,6 +1,12 @@
 window.onload = function () {
     var box = document.getElementById('box');
-    console.log(box);
+    // console.log(box);
+
+    box.style.background = 'red';
+    box.style.border = '10px solid black';
+
+    box.onclick = function(){
+    }
     
 }
 console.log('after onload');
