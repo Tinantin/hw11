5e481ce02b1dcfac15f774c26e7518eb6f3ec9f1
Valentina <valentinalobodane@gmail.com>
2020-01-16 22:24:43 +0200

    menu row

--- main15.js
+++ main15.js
@@ -1,41 +1,50 @@
 window.onload = function () {
 
-   var data = { 
-      name: 'menu', 
-      type: 'row|column', 
-      items: [
-        {
-          title: 'title 1',
-          handler: 'ActionAdd'
-        },
-        {
-          title: 'title 2',
-          handler: 'ActionSaveAs'
-        },
-        {
-          title: 'title 3',
-          handler: 'ActionExit'
-        }
-      ]
-    }
+  var data = { 
+     name: 'menu', 
+     type: 'row', 
+     items: [
+       {
+         title: 'title 1',
+         handler: 'ActionAdd'
+       },
+       {
+         title: 'title 2',
+         handler: 'ActionSaveAs'
+       },
+       {
+         title: 'title 3',
+         handler: 'ActionExit'
+       }
+     ]
+   }
 
 
 
 var menu = document.querySelector('.menu');
 
+var list = document.createElement('ul');
+list.classList.add('row-menu');
+
 for(var i = 0; i < data.items.length; i++) {
-    var div = document.createElement('div');
+   var itemList = document.createElement('li');
 
-    div.classList.add('item');
-    div.innerHTML = data.items[i].title;
-    div.style.border = '5px solid green';
+   itemList.classList.add('item');
+   itemList.innerHTML = data.items[i].title;
+   itemList.style.border = '5px solid green';
+   itemList.style.marginTop = '10px';
 
-    menu.appendChild(div);
-  }
+   list.appendChild(itemList);
+ }
 
-
+ menu.onclick = function(){
+   
 }
-    console.log('after onload');
+
+menu.appendChild(list);
+}
+   console.log('after onload');
+
 
 
 
