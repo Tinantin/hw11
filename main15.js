0ed0c186212180eee791faa51c1275327b759191
Valentina <valentinalobodane@gmail.com>
2020-01-17 17:02:16 +0200

    add function event listener click action

--- main15.js
+++ main15.js
@@ -19,29 +19,38 @@ window.onload = function () {
      ]
    }
 
+  var actions = {
+    ActionAdd: function() {
+      console.log('ActionAdd')
+    },
+    ActionSaveAs: function() {
+      console.log('ActionSaveAs')
+    },
+    ActionExit: function() { 
+      console.log('ActionExit')
+    }
+  }
 
 
-var menu = document.querySelector('.menu');
-
-var list = document.createElement('ul');
-list.classList.add('row-menu');
-
-for(var i = 0; i < data.items.length; i++) {
-   var itemList = document.createElement('li');
-
-   itemList.classList.add('item');
-   itemList.innerHTML = data.items[i].title;
-   itemList.style.border = '5px solid green';
-   itemList.style.marginTop = '10px';
-
-   list.appendChild(itemList);
- }
-
- menu.onclick = function(){
-   
-}
-
-menu.appendChild(list);
+  var menu = document.querySelector('.menu');
+  var list = document.createElement('ul');
+  list.classList.add('row-menu');
+  
+  for(var i = 0; i < data.items.length; i++) {
+      var itemList = document.createElement('li');
+  
+      itemList.style.border = '5px solid green';
+      itemList.style.marginTop = '10px';
+  
+      itemList.classList.add('item');
+      itemList.innerHTML = data.items[i].title;
+      // itemList.onclick = data.items[i].handler;
+  
+      itemList.addEventListener('click', actions[data.items[i].handler]);
+      
+      list.appendChild(itemList);
+    }
+  menu.appendChild(list);
 }
    console.log('after onload');
 
