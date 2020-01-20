42f3b911b4e4339914dc9372e9cfdc64a3476868
Valentina <valentinalobodane@gmail.com>
2020-01-20 15:33:07 +0200

    add function get item from local storage

--- main17.js
+++ main17.js
@@ -2,6 +2,7 @@ window.onload = function () {
 
   var fields = document.querySelectorAll('.firstName, .lastName, .age');
   var button = document.querySelector('#push');
+  getItemValue ();
 
 function setItemValue (){
 ////////////////// положить значения в localStorage
@@ -16,8 +17,13 @@ console.log(obj);
  
   localStorage.setItem('form', JSON.stringify(obj));
 }
-
 button.onclick = setItemValue;
+
+function getItemValue (){
+  ////////////////// получить значения с localStorage
+    var obj = localStorage.getItem('form');
+    console.log(obj);
+}
 }
     console.log('after onload');
 
