fd9661122b7df0ff4b8b9651f0fab0179a151524
Valentina <valentinalobodane@gmail.com>
2020-01-20 15:00:38 +0200

    add button and onclick, fix obj

--- main17.js
+++ main17.js
@@ -1,21 +1,23 @@
 window.onload = function () {
 
-  var fields = this.document.querySelectorAll('.firstName, .lastName, .age')
-  
-  function setItem (){
-  ////////////////// положить значения в localStorage
-    for (var i = 0; i < fields.length; i++){
-      var obj = {}; 
-  
-      var name = fields[i].name;
-      var value = fields[i].value;
-      obj[name] = value;
-    }
-    
-    var json = JSON.stringify(obj);
-    localStorage.setItem('form', json);
+  var fields = document.querySelectorAll('.firstName, .lastName, .age');
+  var button = document.querySelector('#push');
+
+function setItemValue (){
+////////////////// положить значения в localStorage
+var obj = {}; 
+
+  for (var i = 0; i < fields.length; i++){
+    var name = fields[i].name;
+    var value = fields[i].value;
+    obj[name] = value;
   }
-  setItem();
-  }
-      console.log('after onload');
+console.log(obj);
+ 
+  localStorage.setItem('form', JSON.stringify(obj));
+}
+
+button.onclick = setItemValue;
+}
+    console.log('after onload');
 
