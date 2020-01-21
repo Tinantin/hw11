20a21fe0e98f1a370e06f82b094b3229ef05dfc1
Valentina <valentinalobodane@gmail.com>
2020-01-21 22:53:40 +0200

    add functions

--- main18.js
+++ main18.js
@@ -3,13 +3,18 @@ window.onload = function () {
   var button = document.querySelector('.push');
  
   var store = {
+     value: '',
+ 
     setInStore: function(key, value){
      localStorage.setItem(key, value);
     },
     getFromStore: function(key){
-
+       value = localStorage.getItem(key) || 0;
     }
   }
+ 
+  store.getFromStore('counter');
+  button.nextElementSibling.innerHTML = store.value;
  
   button.onclick = function (){
     var value = ++this.nextElementSibling.innerText;
@@ -18,5 +23,7 @@ window.onload = function () {
  
  }
      console.log('after onload');
+ 
+ 
  
  
