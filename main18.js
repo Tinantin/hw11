d0e87c56c0fe36db2fa9de11bc7a5480ea1d6386
Valentina <valentinalobodane@gmail.com>
2020-01-21 21:56:34 +0200

    creat button, add function click

---
+++ main18.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+
+ var button = document.querySelector('.push');
+
+ button.onclick = function (){
+   console.log('click');
+ }
+
+}
+    console.log('after onload');
+
+
+
