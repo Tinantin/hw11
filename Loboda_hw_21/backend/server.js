138d2253c0598c51f6ca761799ecc679efa91783
Valentina <valentinalobodane@gmail.com>
2020-01-31 11:49:59 +0200

    add func read, add json file

--- Loboda_hw_21/backend/server.js
+++ Loboda_hw_21/backend/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {     //разрешаем всем видам запросов работать с нашим сервером
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -9,20 +10,21 @@ app.use(function (req, res, next) {     //разрешаем всем видам
 	next();
 });
 
+app.post('/users', function (req, res) { // req - обьект запроса, res -- обьект ответа  
+  fs.readFile("users.json", 'utf-8', function(err, content) {
+    users = JSON.parse(content);
+    const form = req.body;
+    const userExist = users.filter(user => 
+      user.login == form.login &&
+      user.password == form.password 
+      );
 
+    console.log('Got body:', req.body);
+  
+   
+  });
 
-function getDataFromDataBasse() {
-  return user = [
-    [{"login":"admin", "password": "admin", "id": 1}]
-  ];
-}
-
-// app.get('/user', function (req, res) { // req - обьект запроса, res -- обьект ответа
-//   const users = getDataFromDataBasse();
-//   const lastUser = [users.pop()];
-//   res.send(lastUser);
-// });
-
+});
 
 app.listen(3000, function () { // говорим на каком порту запускать нашу  NODE_JS  программу.
   console.log('Example app listening on port http://localhost:3000/users');
@@ -30,10 +32,6 @@ app.listen(3000, function () { // говорим на каком порту за
 
 
 
-// app.get('/', function (req, res) { // req - обьект запроса, res -- обьект ответа
-    
-//   res.send('ghh');
-// });
 
 
 
