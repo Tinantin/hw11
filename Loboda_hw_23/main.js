b78e1e0afa9cebc4fbdedb54a4d16b94922f8c1c
Valentina <valentinalobodane@gmail.com>
2020-02-04 21:16:52 +0200

    create button, add method transfer for array

--- Loboda_hw_23/main.js
+++ Loboda_hw_23/main.js
@@ -1,10 +1,17 @@
 var app = new Vue({
   el: '#app',
   data: {
+ 
     items: [
       { text: 'bla' },
       { text: 'bla bla' },
       { text: 'bla  bla  bla ' }
     ]
-  }
+  },
+  methods:{
+    transfer(){
+        this.items.pop();
+        
+      }
+  },
 })
