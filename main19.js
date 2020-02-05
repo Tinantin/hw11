8f4280004bd80227f69feebacff2fd5b5d3eb89d
Valentina <valentinalobodane@gmail.com>
2020-02-05 13:54:39 +0200

    add input, add filter for title

--- main19.js
+++ main19.js
@@ -1,53 +1,33 @@
-window.onload = function () {
+let data = [
+  { title: 'Title1', size: 100, img: 'url', cost: 300}, 
+  { title: 'Title2', size: 200, img: 'url',cost: 300}, 
+  { title: 'Title2', size: 150, img: 'url', cost: 300}]
 
-  let data = [
-    { title: 'Title1', size: 100, img: 'url', cost: 300}, 
-    { title: 'Title2', size: 200, img: 'url',cost: 300}, 
-    { title: 'Title2', size: 150, img: 'url', cost: 300}]
-
-  const block = document.querySelector('.block')
-
-      function ViewController (){
-
-      }
-      ViewController.prototype.render = mass => {
-        let items = mass.map(item => '<span' + ' class="block"' + '>' + 
-        item.title + '<br>' +
-        item.size + '<br>' +
-        item.img + '<br>' +
-        item.cost + '<br>' +
-          '</span>');
-        block.innerHTML  = items.join(' ')
-      }
-
-      let viev = new ViewController();
-
-      // console.log(viev);
-
-      viev.render(data);
+const block = document.querySelector('.block');
+var titleInput = document.getElementById('title');
 
 
-      // var card = document.querySelector('.block');
-      // var list = document.createElement('div');
+    function ViewController (){
 
-      // for(var i = 0; i <obj.length; i++) {
+    }
+    ViewController.prototype.render = mass => {
+      let items = mass.map(item => '<div' + ' class="block"' + '>' + 
+      '<strong>' + 'Title:'+ '</strong>' + ' ' + item.title + '<br/>' +
+      '<strong>' + 'Size:'+ '</strong>' + ' ' + item.size + '<br/>' +
+      '<strong>' + 'Image:'+ '</strong>' + ' ' + item.img + '<br/>' +
+      '<strong>' + 'Cost:'+ '</strong>' + ' ' + item.cost + '<br/>' +
+        '</div>');
+      block.innerHTML  = items.join('<br/>');
 
-      //   var itemList = document.createElement('div');
+      
+    }
 
-      //   itemList.innerHTML += '<span>'+ 'Title:'+ ' ' + obj[i].title + ' ' + '</span>';
-      //   itemList.innerHTML += '<span>' + 'Size:'+ ' ' + obj[i].size + ' ' + '</span>';
-      //   itemList.innerHTML += '<span>' + 'Image:'+ ' ' + obj[i].img + ' ' + '</span>';
-      //   itemList.innerHTML += '<span>' + 'Cost:'+ ' ' + obj[i].cost + ' ' + '</span>';
-
-      //   itemList.style.border = '5px solid gray';
-      //   itemList.style.marginTop = '10px';
-
-      //   list.appendChild(itemList);
-      // }
-      // card.appendChild(list);
-
-}
-    console.log('after onload');
-
-
-
+    let viev = new ViewController();
+    viev.render(data);
+    
+    titleInput.oninput = function() {
+      let items = data.filter(item => 
+        item.title.includes(titleInput.value)); 
+      console.log(items);
+      viev.render(items);
+    };
