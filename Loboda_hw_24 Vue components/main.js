2f994277d18bd677c2beade78a8b49d8bc4b8cc7
Valentina <valentinalobodane@gmail.com>
2020-02-21 11:35:25 +0200

    change template, add local storage

--- Loboda_hw_24 Vue components/main.js
+++ Loboda_hw_24 Vue components/main.js
@@ -1,99 +1,107 @@
 
 Vue.component('app-div', {
-  props:  {
-      obj: Object,
-      // className: String
+  props: {
+    obj: Object
   },
   data() {
     return {
-      sizeRender: 'inputs',
-      dataObj: obj
+      sizeRender: '',
+      columnName: ''
     }
   },
-  computed:{
-    activeColor: function(){
-        return {
-            'background-color': this.obj.activeColor
-        }
-      },
-      sizeStyle: function(){
-          return {
-              height: this.sizeRender + 'px'
-          }
-    }
-  },
-  created: function (){
-      this.sizeRender = this.obj.height  
-    },
-    mounted() {
-      if (this.dataObj == this.obj) {
-        this.dataObj = localStorage.dataObj;
-      }
-    },
-    watch: {
-      dataObj(newObj) {
-        localStorage.dataObj = newObj;
-      }
-    },
-  
-  template: `
-  
-  <div :class="[obj.className]" v-model="dataObj">
-    <div class="wrapper" :style = "[activeColor, sizeStyle]">
-          {{obj.name}}
-     </div>
-     <input type="range" min="0" :max="obj.height" step="1" v-model="sizeRender"> 
-  </div>
-  
-`
-})
-
-Vue.component('diagram', {
-  data() {
+  computed: {
+    activeColor: function () {
       return {
-          divs: [
-            {name: 'Column 1', className: 'block', activeColor: 'red', height: 220, },
-            {name: 'Column 2', className: 'block', activeColor: 'blue', height: 200},
-            {name: 'Column 3', className: 'block', activeColor: 'green', height: 100},
-            {name: 'Column 4', className: 'block', activeColor: 'gray', height: 170},
-            {name: 'Column 5', className: 'block', activeColor: 'yellow', height: 230},
-            {name: 'Column 6', className: 'block', activeColor: 'pink', height: 270},
-            {name: 'Column 7', className: 'block', activeColor: 'purple', height: 300}
-          ]
+        'background-color': this.obj.activeColor
       }
+    },
+    sizeStyle: function () {
+      let curObj = {
+        columnName: this.obj.name,
+        height: this.sizeRender,
+        width: this.sizeRender,
+      };
+
+      this.$emit('resize', curObj);
+      return {
+        height: this.sizeRender + 'px',
+        width: this.sizeRender + 'px',
+      }
+    }
+  },
+  created: function () {
+    this.sizeRender = this.obj.width;
+    this.columnName = this.obj.name;
   },
   template: `
-      <div class="diagram">
-          <app-div 
-              v-for="divItems in divs" 
-              :obj="divItems"
-              />
-              
+  
+      <div :class="[obj.className]" >
+        <div class="wrapper" :style = "[activeColor, sizeStyle]" >
+        {{obj.name}}
+         </div>
+         <input type="range" min="0" max="300" step="1" v-model="sizeRender"> 
       </div>
+      
   `
-});
-
+})
 
 
 Vue.component('main-app', {
   data() {
-      return {
-          appName: 'Hello world'
-      }
+    return {
+      divs: [
+        { name: 'Column 1', className: 'block', activeColor: 'red', height: 250, width: 250 },
+        { name: 'Column 2', className: 'block', activeColor: 'blue', height: 200, width: 200 },
+        { name: 'Column 3', className: 'block', activeColor: 'green', height: 100, width: 50 },
+        { name: 'Column 4', className: 'block', activeColor: 'gray', height: 210, width: 150 },
+        { name: 'Column 5', className: 'block', activeColor: 'yellow', height: 330, width: 350 },
+        { name: 'Column 6', className: 'block', activeColor: 'pink', height: 270, width: 340 },
+        { name: 'Column 7', className: 'block', activeColor: 'purple', height: 330, width: 330 }
+      ],
+      items: []
+    }
+  },
+  methods: {
+    readLocalStorage() {
+      return JSON.parse(localStorage.getItem('app-data'));
+    },
+    writeLocalStorage(data) {
+      localStorage.setItem('app-data', JSON.stringify(data));
+    },
+    changeSize(item) {
+      const index = this.items.map(e => e.name).indexOf(item.columnName);
+      this.items[index].height = item.height;
+      this.items[index].width = item.width;
+      this.writeLocalStorage(this.items);
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('app-data')) {
+      this.items = this.readLocalStorage();
+    } else {
+      this.items = this.divs;
+      this.writeLocalStorage(this.divs);
+    }
+    // console.log(this.readLocalStorage());
   },
   template: `
   <div>
       <header>
-          <diagram />
-         
+      <div class="diagram">
+        <app-div 
+            v-for="item in items" 
+            :obj="item"
+            v-on:resize="changeSize"
+            />
+        </div>
       </header>
   </div>
     `
-}); 
+});
 
 new Vue({
-  el:'#app',
+  el: '#app',
   data: {
-    
+
   }
 });
