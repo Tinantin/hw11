e22367363e15ec2a74bb0d425543e86673aac8ed
Valentina <valentinalobodane@gmail.com>
2020-02-25 22:55:10 +0200

    add inputs

--- hw11/main.js
+++ hw11/main.js
@@ -1,5 +1,6 @@
 function SuperMath() {
     this.obj = {};
+    x = prompt("Enter X");
 }
 SuperMath.prototype.sum = function (a, b) {
   alert(a + b);
@@ -22,6 +23,10 @@ SuperMath.prototype.mul = function (a, b) {
 SuperMath.prototype.mod = function (a, b) {
   alert(a % b);
 }
-
+SuperMath.prototype.input = function(){
+  this.obj.X = prompt('Введите число X: ',0);
+  this.obj.Y = prompt('Введите число Y: ',0);
+  this.obj.znak = prompt('Введите znak(возможные варианты znak=>  `+ - / * %`): ', "/");
+}
 
 SuperMath();
