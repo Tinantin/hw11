e1634b1bf761d6358f91534485500940e213252e
Valentina <valentinalobodane@gmail.com>
2020-02-27 11:16:53 +0200

    create box

--- Loboda_hw_14/main.js
+++ Loboda_hw_14/main.js
@@ -1,8 +1,7 @@
 window.onload = function () {
 
   var box = document.querySelector('#box'), curPos = 0;
-  var push = true;
-
+  
   box.style.background = 'red';
   box.style.border = '10px solid black';
 
