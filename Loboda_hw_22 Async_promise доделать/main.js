1d80247964abd332240c59f986de499d0807ae92
Valentina Loboda <valentinalobodane@gmail.com>
2020-03-15 22:02:15 +0200

    add promise ladder

--- Loboda_hw_22 Async_promise доделать/main.js
+++ Loboda_hw_22 Async_promise доделать/main.js
@@ -12,5 +12,51 @@ window.onload = function () {
     }, 1000);
   })
 
-
+  promise.then(function () {
+    console.log(1);
+    return id;
+  },
+    function () {
+      console.log(2);
+      return id;
+    })
+    .then(function () {
+      console.log(3);
+      return Promise.reject(id);
+    },
+      function () {
+        console.log(4);
+      })
+    .then(function () {
+      console.log(5);
+    },
+      function () {
+        console.log(6);
+        if (id) {
+          return Promise.reject(id);
+        } else {
+          return id;
+        }
+      })
+    .then(function () {
+      console.log(7);
+      return id;
+    },
+      function () {
+        console.log(8);
+        return id;
+      })
+    .then(function () {
+      console.log(9);
+      return Promise.reject(id);
+    },
+      function () {
+        console.log(10);
+      })
+    .then(function () {
+      console.log(11);
+    },
+      function () {
+        console.log(12);
+      })
 }
