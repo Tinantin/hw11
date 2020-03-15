027dfdf4609964b56db03def5f177a90a5cc3e06
Valentina Loboda <valentinalobodane@gmail.com>
2020-03-15 22:00:11 +0200

    add setimeout id

--- Loboda_hw_22 Async_promise доделать/main.js
+++ Loboda_hw_22 Async_promise доделать/main.js
@@ -1,7 +1,15 @@
 window.onload = function () {
 
   const promise = new Promise(function (resolve, reject) {
-
+    console.log(0);
+    setTimeout(function () {
+      const id = prompt('ID');
+      if (id) {
+        resolve(id)
+      } else {
+        reject(id)
+      }
+    }, 1000);
   })
 
 
